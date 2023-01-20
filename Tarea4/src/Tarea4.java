be4fc4b2df1e3f1afe50994f14da9f2aec042727
EPPalom <a_eva.palomo.palomas@iespablopicasso.es>
2023-01-20 14:35:14 +0100

    Primer commit

---
+++ Tarea4/src/Tarea4.java
@@ -0,0 +1,14 @@
+
+public class Tarea4 {
+
+  public static void main(String[] args) {
+    
+    int[] numeros = new int[20];
+    
+    for (int i = 0; i < 20; i++) {
+      numeros[i] = (int)(Math.random()*10+1);
+    }
+
+  }
+
+}
